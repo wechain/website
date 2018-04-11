f877f7ac06bf46178d3ee3a96b46d00ef48a0272
NataliaTepluhina <tarya.se@gmail.com>
2018-04-11 16:39:24 +0300

    * added Connect.Tech event

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,6 @@ import Sponsor from '@/components/Sponsor'
 import Credits from '@/components/Credits'
 import Event from '@/components/Events/EventTemplate'
 import NotFoundComponent from '@/components/NotFoundComponent'
-import WeRise2018 from '@/components/Events/WeRise2018'
-import VueUS2018 from '@/components/Events/VueUS2018'
-import ConnectTech2018 from '@/components/Events/ConnectTech2018'
 
 export default new Router({
   routes: [
@@ -40,21 +37,6 @@ export default new Router({
     {
       path: '/events/:event',
       component: Event,
-    },
-    {
-      path: '/WeRise2018',
-      name: 'werise2018',
-      component: WeRise2018
-    },
-    {
-      path: '/VueUS2018',
-      name: 'vueus2018',
-      component: VueUS2018
-    },
-    {
-      path: '/ConnectTech2018',
-      name: 'connecttech2018',
-      component: ConnectTech2018
     },
     {
       path: '/home',
