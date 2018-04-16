dcaebc34219c9bb2694332b989a7d1ff22e33588
NataliaTepluhina <tarya.se@gmail.com>
2018-04-16 17:12:00 +0300

    * styled calendar appearance

--- src/views/Upcoming.vue
+++ src/views/Upcoming.vue
@@ -2,7 +2,7 @@
   <b-container fluid>
     <b-row>
       <b-col md="8" offset-md="2">
-        <article class="card">
+        <article class="card upcoming-card">
           <img src="../assets/skulk.jpg" class="card-img-top"/>
           <div class="card-body">
             <h4 class="title">Upcoming Skulks</h4>
@@ -10,15 +10,16 @@
               What's a skulk? A group of foxes is actually called a skulk. Here are events coming soon to
               a conference near you!
             </p>
-            <div class="calendar" v-if="!!story.content && !!story.content.body" align="center">
+            <div class="full-width" v-if="!!story.content && !!story.content.body" align="center">
               <b-button class="toggle-button" size="lg" variant="success" @click="showGrid = !showGrid">
                 {{showGrid ? 'Show calendar' : 'Show grid'}}
               </b-button>
               <transition name="fade" mode="out-in" appear>
-                <vv-grid v-if="showGrid" :events="story.content.body"></vv-grid>
-                <div class="calendar" v-else>
+                <vv-grid class="grid" v-if="showGrid" :events="story.content.body"></vv-grid>
+                <div class="calendar full-width" v-else>
                   <calendar-view :show-date="showDate"
                                  :events="events"
+                                 disablePast
                                  @show-date-change="setShowDate"
                                  @click-event="navigateToEvent"></calendar-view>
                 </div>
@@ -63,8 +64,8 @@
       }
     },
     methods: {
-      setShowDate(d) {
-        this.showDate = d;
+      setShowDate(date) {
+        this.showDate = date;
       },
       navigateToEvent(event) {
         this.$router.push(event.originalEvent.link);
@@ -82,11 +83,92 @@
     margin-bottom: 20px;
   }
 
-  .calendar {
+  .full-width {
     width: 100%;
   }
 
+  .calendar-view {
+    .header {
+      .nav {
+        button {
+          cursor: pointer;
+          &:focus {
+            outline: none;
+          }
+          &:hover {
+            background-color: #b5e2cd;
+            &:disabled {
+              background-color: #f7f7f7;
+              cursor: default;
+            }
+          }
+        }
+      }
+    }
+    .weeks {
+      .day.today {
+        background-color: lightblue;
+      }
+    }
+  }
+
+  .calendar {
+    padding-bottom: 40px;
+  }
   .fade-enter-active, .fade-leave-active {
     transition: opacity 0.5s;
+  }
+
+  @media (max-width: 768px) {
+    .upcoming-card {
+      .card-body {
+        padding: 0;
+      }
+      .title, .card-text, .grid {
+        padding: 0 1.25rem;
+      }
+      .title {
+        padding-top: 1.25rem;
+      }
+    }
+    .conference {
+      visibility: hidden;
+      position: relative;
+      margin-top: -4px;
+      height: 28px;
+      &:before {
+        position: absolute;
+        visibility: visible;
+        content: '';
+        height: 26px;
+        width: 26px;
+        top: 0;
+        left: 2px;
+        -webkit-border-radius: 26px;
+        -moz-border-radius: 26px;
+        border-radius: 26px;
+        background-image: url('../assets/fox-mini.png');
+        background-size: cover;
+      }
+    }
+    .calendar-view {
+      .header {
+        .nav {
+          button {
+            cursor: pointer;
+            &:focus {
+              outline: none;
+            }
+            &:hover {
+              background-color: buttonface;
+              &:disabled {
+                background-color: #f7f7f7;
+                cursor: default;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 </style>
