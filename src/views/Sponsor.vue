b47136d8454e7421fd231431fcc055f9ea559f88
jlooper <jen.looper@gmail.com>
2018-04-17 15:17:03 -0400

    converting last form from Firebase to Typeform

--- src/views/Sponsor.vue
+++ src/views/Sponsor.vue
@@ -1,5 +1,4 @@
 <template>
-  <b-form>
     <b-container fluid>
       <b-row align-h="start">
         <b-col md="8" offset-md="2">
@@ -21,136 +20,17 @@
                 <li>🦊 Mentors drawn from conference speakers. We will work to recruit them if you can provide a list!
                 </li>
               </ul>
-              <b-alert v-if="message" show variant="success">{{message}}</b-alert>
-              <form v-on:submit.prevent="sendRequest">
-                <vue-form-generator class="form" :model="model" :schema="schema"
-                                    :options="formOptions"></vue-form-generator>
-              </form>
+              <p align="center">
+                  <b-button class="btn toggle-button btn-success btn-lg" size="lg" href="https://ladeezfirstmedia.typeform.com/to/dlOUZe">Request a Skulk</b-button>                 
+                </p>
+               
             </div>
           </div>
         </b-col>
       </b-row>
     </b-container>
-  </b-form>
+  
 </template>
-<script>
-  import VueFormGenerator from 'vue-form-generator'
-
-  import {db} from '../firebase';
-
-  const dbRef = db.ref('requests');
-
-  export default {
-    name: 'Form',
-    components: {
-      'vue-form-generator': VueFormGenerator.component
-    },
-    data() {
-      return {
-        message: '',
-        model: {
-          firstName: '',
-          lastName: '',
-          eventName: '',
-          eventLocation: '',
-          eventDate: '',
-          Email: '',
-          message: ''
-        },
-        schema: {
-          fields: [
-            {
-              type: 'input',
-              inputType: 'text',
-              label: 'First Name',
-              model: 'firstName',
-              maxlength: 50,
-              required: true
-            },
-            {
-              type: 'input',
-              inputType: 'text',
-              label: 'Last Name',
-              model: 'lastName',
-              maxlength: 50,
-              required: true
-            },
-            {
-              type: 'input',
-              inputType: 'text',
-              label: 'Event Name',
-              model: 'eventName',
-              maxlength: 100,
-              required: true
-            },
-            {
-              type: 'input',
-              inputType: 'text',
-              label: 'Event Location',
-              model: 'eventLocation',
-              maxlength: 50,
-              required: true
-            },
-            {
-              type: 'input',
-              inputType: 'text',
-              label: 'Event Date',
-              model: 'eventDate',
-              maxlength: 50,
-              required: true
-            },
-            {
-              type: 'input',
-              inputType: 'email',
-              label: 'Email',
-              model: 'Email',
-              maxlength: 50,
-              required: true
-            },
-            {
-              type: 'submit',
-              inputType: 'submit',
-              buttonText: 'Submit',
-              onSubmit: 'sendRequest',
-              validateBeforeSubmit: false
-            }
-          ]
-        },
-
-        formOptions: {
-          validateAfterLoad: true,
-          validateAfterChanged: true
-        }
-      }
-    },
-    methods: {
-      sendRequest: function () {
-        dbRef.push({
-          firstName: this.model.firstName,
-          lastName: this.model.lastName,
-          eventName: this.model.eventName,
-          eventLocation: this.model.eventLocation,
-          eventDate: this.model.eventDate,
-          email: this.model.Email
-        }).then(() => {
-          //clear form
-          this.message = 'Thank you for your request! We will review it and get back to you soon.',
-            this.model.firstName = '',
-            this.model.lastName = '',
-            this.model.eventName = '',
-            this.model.eventLocation = '',
-            this.model.eventDate = '',
-            this.model.Email = ''
-        }).then(() => {
-          //this.message = ''
-        })
-
-        //todo send message back and clear form
-
-      }
-    }
-  }
-</script>
 
 <style lang="scss">
   input[type=submit] {
