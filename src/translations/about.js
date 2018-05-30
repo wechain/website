87a3c0a22f2548d844b6f945cbb35fd123045679
NataliaTepluhina <tarya.se@gmail.com>
2018-05-30 19:29:53 +0300

    * about page localized

---
+++ src/translations/about.js
@@ -0,0 +1,67 @@
+export default {
+  en: {
+    vixens: {
+      title: 'About Vue Vixens',
+      content: `{medium} is an initiative founded by {jen}, Developer Advocate at
+              {progress}, that creates and hosts workshops to teach
+              Vue.js to under-represented people in a cool and fun way.
+              It is based on the successful model pioneered by Shmuela Jacobs for the Angular
+              community ({ngGirls}) who was in turn
+              inspired by the Rails Bridge and Django Girls initiatives.
+              The format involves self-driven code labs completed in a workshop format
+              in groups with mentoring by conference-goers and speakers who volunteer to help.
+              The goal of the program is to familiarize women and those who identify as such
+              with Vue.js in a supportive and inclusive environment. We also help conferences organize
+              their diversity initiatives such as offering free conference tickets, and are building a scholarship
+              fund to help offset those attendees' conference costs so that they can more easily attend.
+              Questions? Contact us at {mail}`,
+      mediumLink: 'Vue Vixens',
+      jenLink: 'Jen Looper',
+      progressLink: 'Progress',
+      ngGirlsLink: 'ng-girls.org',
+      mailLink: 'info@vuevixens.org',
+    },
+    activities: {
+      title: 'About Our Activities',
+      content: {
+        paragraph1: `What can you expect from a Vue Vixens event? Well, as a student, perhaps new
+              to the field of frontend development, you are given the opportunity to learn
+              about Vue.js, a cool, fresh framework for building both web and mobile apps. Over
+              the course of a breakfast, a lunch 'n' learn, or a full day, you'll work with a
+              mentor in small groups in a self-driven workshop.
+              You'll complete the task of building
+              a web app and a mobile app that will teach you about the framework and programming in
+              JavaScript.
+              You'll also get a chance to meet and work with like-minded students and interface with
+              leaders in the industry - our mentors are often speakers at the conference you attend.`,
+        paragraph2: `{mentorsNote}: if you're joining a Vue Vixens skulk as a mentor, firstly, thank
+              you! Because of you, we can offer
+              free workshops to new developers in a supportive environment. Your job is to stay with your
+              mentees throughout
+              the day, helping them over any difficulties and giving them a view of the life of a seasoned
+              professional
+              developer. You're the best!
+              Just to be clear, anyone can be a mentor for Vue Vixens! We are grateful for your participation.`,
+        mentorsNote: `A note to mentors`,
+        paragraph3: `{studentsNote}: unsure if you will fit into a Vue Vixens skulk? If you are a new
+              Vue.js developer who uses the pronouns "she" or "they", you
+              are welcome. We hate to leave anyone out, but this group is for women, and those who
+              identify as such. We are trans-friendly.
+              The founder of Vue Vixens, Jen Looper, is a Wellesley College Alumna (class of '92) and
+              takes the college's admission criteria
+              as a blueprint. To paraphrase their statement of admissibility:`,
+        studentsNote: 'A note to students',
+        paragraph4: `"We will admit any person who lives as a woman and consistently identifies as a woman;
+              therefore, candidates assigned male at birth
+              who identify as women are eligible. We also welcome those who were assigned female at birth
+              and who feel they belong in our community of women.
+              We seek to provide students with a uniquely empowering learning environment—one designed
+              specifically to prepare women to thrive in a complex world."`,
+        paragraph5: 'Thank you for understanding! 🦊',
+      }
+    }
+  },
+  es: {
+
+  }
+};
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,72 +1,46 @@
 <template>
-    <b-container fluid>
-        <b-row>
-            <b-col md="8" offset-md="2">
-                <article class="card">
-                    <div class="card-body">
-                        <p align="center"><img src="../assets/logo.png"/></p>
-                        <h4 class="title">About Vue Vixens</h4>
-                        <p align="left">
-                            <a href="https://medium.com/@jenlooper/introducing-vue-vixens-82a36455438a">Vue Vixens</a> is an initiative founded by <a href="http://www.jenlooper.com">Jen Looper</a>, Developer Advocate at 
-                            <a href="http://www.progress.com">Progress</a>, that creates and hosts workshops to teach 
-                            Vue.js to under-represented people in a cool and fun way. 
-                            It is based on the successful model pioneered by Shmuela Jacobs for the Angular 
-                            community (<a href="http://www.ng-girls.org">ng-girls.org</a>) who was in turn 
-                            inspired by the Rails Bridge and Django Girls initiatives. 
-                            The format involves self-driven code labs completed in a workshop format 
-                            in groups with mentoring by conference-goers and speakers who volunteer to help. 
-                            The goal of the program is to familiarize women and those who identify as such 
-                            with Vue.js in a supportive and inclusive environment. We also help conferences organize 
-                            their diversity initiatives such as offering free conference tickets, and are building a scholarship 
-                            fund to help offset those attendees' conference costs so that they can more easily attend. 
-                            Questions? Contact us at <a href="mailto:info@vuevixens.org">info@vuevixens.org</a>
-                        </p>
-                        <h4 class="title">About Our Activities</h4>
-                        <p align="left">
-                            What can you expect from a Vue Vixens event? Well, as a student, perhaps new
-                            to the field of frontend development, you are given the opportunity to learn
-                            about Vue.js, a cool, fresh framework for building both web and mobile apps. Over
-                            the course of a breakfast, a lunch 'n' learn, or a full day, you'll work with a 
-                            mentor in small groups in a self-driven workshop.
-                            You'll complete the task of building
-                            a web app and a mobile app that will teach you about the framework and programming in
-                            JavaScript.
-                            You'll also get a chance to meet and work with like-minded students and interface with
-                            leaders in the industry - our mentors are often speakers at the conference you attend.
-                        </p>
-                        <p align="left">
-                            <b>A note to mentors</b>: if you're joining a Vue Vixens skulk as a mentor, firstly, thank
-                            you! Because of you, we can offer
-                            free workshops to new developers in a supportive environment. Your job is to stay with your
-                            mentees throughout
-                            the day, helping them over any difficulties and giving them a view of the life of a seasoned
-                            professional
-                            developer. You're the best!
-                            Just to be clear, anyone can be a mentor for Vue Vixens! We are grateful for your participation.
-                        </p>
-                        <p align="left">
-                            <b>A note to students</b>: unsure if you will fit into a Vue Vixens skulk? If you are a new
-                            Vue.js developer who uses the pronouns "she" or "they", you
-                            are welcome. We hate to leave anyone out, but this group is for women, and those who
-                            identify as such. We are trans-friendly.
-                            The founder of Vue Vixens, Jen Looper, is a Wellesley College Alumna (class of '92) and
-                            takes the college's admission criteria
-                            as a blueprint. To paraphrase their statement of admissibility:
-                        </p>
-                        <p align="left">
-                            "We will admit any person who lives as a woman and consistently identifies as a woman;
-                            therefore, candidates assigned male at birth
-                            who identify as women are eligible. We also welcome those who were assigned female at birth
-                            and who feel they belong in our community of women.
-                            We seek to provide students with a uniquely empowering learning environment—one designed
-                            specifically to prepare women to thrive in a complex world."
-                        </p>
-                        <p align="left">
-                            Thank you for understanding! 🦊
-                        </p>
-                    </div>
-                </article>
-            </b-col>
-        </b-row>
-    </b-container>
+  <b-container fluid>
+    <b-row>
+      <b-col md="8" offset-md="2">
+        <article class="card">
+          <div class="card-body">
+            <p align="center"><img src="../assets/logo.png"/></p>
+            <h4 class="title">{{ $t("vixens.title") }}</h4>
+            <i18n path="vixens.content" tag="p" align="left">
+              <a href="https://medium.com/@jenlooper/introducing-vue-vixens-82a36455438a"
+                 place="medium">{{ $t("vixens.mediumLink") }}</a>
+              <a href="http://www.jenlooper.com"
+                 place="jen">{{ $t("vixens.jenLink") }}</a>
+              <a href="http://www.progress.com"
+                 place="progress">{{ $t("vixens.progressLink") }}</a>
+              <a href="http://www.ng-girls.org"
+                 place="ngGirls">{{ $t("vixens.ngGirlsLink") }}</a>
+              <a href="mailto:info@vuevixens.org"
+                 place="mail">{{ $t("vixens.mailLink") }}</a>
+            </i18n>
+            <h4 class="title">{{ $t("activities.title") }}</h4>
+            <p align="left">{{ $t("activities.content.paragraph1") }}</p>
+            <i18n path="activities.content.paragraph2" tag="p" align="left">
+              <b place="mentorsNote">{{ $t("activities.content.mentorsNote") }}</b>
+            </i18n>
+            <i18n path="activities.content.paragraph3" tag="p" align="left">
+              <b place="studentsNote">{{ $t("activities.content.studentsNote") }}</b>
+            </i18n>
+            <p align="left">{{ $t("activities.content.paragraph4") }}</p>
+            <p align="left">{{ $t("activities.content.paragraph5") }}</p>
+          </div>
+        </article>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+
+<script>
+  import messages from '../translations/about'
+
+  export default {
+    i18n: {
+      messages
+    }
+  }
+</script>
