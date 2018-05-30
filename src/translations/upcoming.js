8841325413c1bf3af87a155f1f83cb29e959c812
NataliaTepluhina <tarya.se@gmail.com>
2018-05-30 19:35:11 +0300

    * upcoming page localized

---
+++ src/translations/upcoming.js
@@ -0,0 +1,10 @@
+export default {
+  en: {
+    title: 'Upcoming Skulks',
+    subtitle: `What's a skulk? A group of foxes is actually called a skulk. Here are events coming soon to
+              a conference near you!`
+  },
+  es: {
+
+  }
+};
