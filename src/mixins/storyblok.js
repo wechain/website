d04fe256521eb4eabb6c1913519a9bc70ae0dbb8
NataliaTepluhina <tarya.se@gmail.com>
2018-06-02 18:06:26 +0300

    * fixed console errors and made English content 'root'

--- src/mixins/storyblok.js
+++ src/mixins/storyblok.js
@@ -15,16 +15,13 @@ export default {
     this.$storyblok.pingEditor(() => {
       this.loadStory(this.$storyblok.inEditor ? 'draft' : 'published', this.$i18n.locale)
     });
-    this.$eventBus.$on('changeLocale', locale => {
-      console.log(this.slug);
-      this.loadStory('published', locale)
-    });
+    this.$eventBus.$on('changeLocale', this.listener);
   },
   methods: {
     loadStory(version, locale) {
       this.loading = true;
       this.$storyblok.get({
-        slug: locale ? `${locale}/${this.slug}`: `en/${this.slug}`,
+        slug: !locale || locale === 'en' ? `${this.slug}`: `${locale}/${this.slug}`,
         version: version
       }, (data) => {
         this.story = {
@@ -37,9 +34,12 @@ export default {
       }, (error) => {
         this.loading = false;
       });
+    },
+    listener(locale) {
+      this.loadStory('published', locale)
     }
   },
   beforeDestroy() {
-    this.$eventBus.$off();
+    this.$eventBus.$off('changeLocale', this.listener);
   }
 }
