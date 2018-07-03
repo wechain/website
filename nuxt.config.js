628b02e29de3b34a2d428dea0976cc03d756f484
NataliaTepluhina <tarya.se@gmail.com>
2018-07-03 22:36:38 +0300

    * scaffolded nuxt project

---
+++ nuxt.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  head: {
+    title: 'Vue Vixens - Workshops for Foxy People to Learn Vue.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Vue Vixens - Workshops for Foxy People to Learn Vue.js' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ]
+  },
+  plugins: ['~/plugins/vuetify'],
+  css: [
+    '~/assets/css/app.styl'
+  ],
+  loading: { color: '#3B8070' },
+  build: {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <div>
+      <v-btn color="teal" light>TEST!</v-btn>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+</style>
