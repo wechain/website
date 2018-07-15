a4e3c21f7cd50acfb438fe2b231af91bd2f7878c
NataliaTepluhina <tarya.se@gmail.com>
2018-07-15 22:09:39 +0300

    * abstracted grid

--- pages/Upcoming.vue
+++ pages/Upcoming.vue
@@ -4,29 +4,7 @@
       <v-flex xs12 class="text-xs-center">
         <h2 class="vv-subheading font-lato text-xs-center">{{ $t("title") }}</h2>
         <p class="vv-content title">{{ $t("subtitle") }}</p>
-        <v-container grid-list-md v-if="!!story.content">
-          <v-layout wrap>
-            <v-flex xs6 sm4 md3 v-for="event in story.content.body" :key="event.name">
-              <v-card class="vv-card" hover :to="event.link.cached_url">
-                <v-card-media
-                  class="white--text vv-card-image"
-                  height="200px"
-                  :src="event.img">
-                  <v-container fill-height fluid>
-                    <v-layout fill-height>
-                      <v-flex xs12 d-flex align-center justify-center class="event-name">
-                        <div>
-                          <h3 class="headline">{{event.location}}</h3>
-                          <p class="title">{{event.date | moment('MMM Do')}}</p>
-                        </div>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-card-media>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
+        <VVGrid v-if="!!story.content" :events="story.content.body" />
       </v-flex>
     </v-container>
   </v-layout>
@@ -35,8 +13,12 @@
 <script>
   import storyblok from '../mixins/storyblok'
   import messages from '../assets/translations/upcoming'
+  import VVGrid from '../components/Grid'
   export default {
     mixins: [storyblok],
+    components: {
+      VVGrid,
+    },
     data() {
       return {
         slug: 'upcoming',
@@ -51,36 +33,6 @@
 <style scoped lang="scss">
   .vv-content {
     padding-top: 30px;
-  }
-
-  .headline, .subheading {
-    width: 100%;
-  }
-
-  .vv-card {
-    &:before {
-      position: absolute;
-      content: '';
-      z-index: 1;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background-color: rgba(0, 0, 0, .6);
-      opacity: 1;
-      transition: 0.3s opacity;
-    }
-
-    &:hover {
-      &:before {
-        opacity: 0.4;
-      }
-    }
-  }
-
-  .event-name {
-    z-index: 2;
-    flex-wrap: wrap;
   }
 
   .container.indexed {
