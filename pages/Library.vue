134e1053352f797637776d9629dd20ddd61a1c5a
NataliaTepluhina <tarya.se@gmail.com>
2018-07-21 22:26:07 +0300

    * Library page

--- pages/Library.vue
+++ pages/Library.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    {{story.name}}
-  </div>
+  <v-layout class="vv-container" fill-height>
+    <v-container class="indexed" v-if="!!story.content" grid-list-md>
+      <v-card class="article-card" hover :href="article.link.url" target="_blank" raised
+              v-for="article in story.content.body" :key="article.title">
+        <v-card-title>
+          <h3 class="d-block text-xs-center">{{article.title}}</h3>
+          <p class="primary--text text--darken-1"><em>{{article.author}}</em></p>
+          <p>{{article.description}}</p>
+        </v-card-title>
+      </v-card>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
@@ -15,3 +24,13 @@
     },
   }
 </script>
+
+<style scoped>
+  .article-card {
+    margin-bottom: 15px;
+  }
+
+  h3, p {
+    width: 100%;
+  }
+</style>
