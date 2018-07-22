ce61f40c2fcf3c4527f59c2c8ee6bfa2e7520d74
NataliaTepluhina <tarya.se@gmail.com>
2018-07-22 18:29:08 +0300

    * event page done

--- pages/events/_event.vue
+++ pages/events/_event.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>
-    {{story.name}}
-  </div>
+  <v-layout class="vv-container" fill-height>
+    <v-container class="indexed">
+      <v-flex xs12 class="text-xs-center" v-if="currentEvent">
+        <v-card height="100%">
+          <img :src="currentEvent.image" :alt="`${currentEvent.title} image`">
+          <v-card-title>
+            <h3 class="vv-subheading font-lato text-xs-center">{{ currentEvent.title }}</h3>
+            <div class="card-meta">
+              <p v-if="currentEvent.conference.url">
+                <i class="fas fa-link"></i>
+                <a :href="currentEvent.conference.url">{{currentEvent.conference.url}}</a>
+              </p>
+              <p v-if="currentEvent.date">
+                <i class="far fa-calendar-alt"></i>
+                {{currentEvent.date| moment('MMM Do, YYYY')}}
+              </p>
+              <p v-if="currentEvent.location">
+                <i class="fas fa-map-marker-alt"></i>
+                {{currentEvent.location}}
+              </p>
+            </div>
+            <p class="text-xs-left">{{currentEvent.description}}</p>
+          </v-card-title>
+          <v-card-actions class="card-buttons" justify-center>
+            <v-btn color="secondary darken-2" dark v-for="action in currentEvent.actions"
+                   :key="action.text"
+                   :href="action.url.url" target="_blank">
+              {{action.text}}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
@@ -18,3 +49,43 @@
     },
   }
 </script>
+
+<style scoped lang="scss">
+  img {
+    width: 100%;
+  }
+
+  .vv-container {
+    padding-bottom: 0;
+  }
+
+  .container.indexed {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  h3 {
+    width: 100%;
+  }
+
+  .card-meta {
+    color: #909090;
+    padding: 20px 0;
+    text-align: left;
+    a {
+      text-decoration: none;
+    }
+
+    i {
+      padding-right: 5px;
+    }
+    p {
+      margin: 0;
+    }
+  }
+
+  .card-buttons {
+    padding-bottom: 50px;
+    justify-content: center;
+  }
+</style>
